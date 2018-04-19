a671e163318ed7e7db69c7908dfe9997807d4155
[a518291] Michel Daviot <michel.daviot@atos.net>
2018-04-19 13:12:00 +0200

    :white_check_mark: rover rotates left

--- test/TestMarsRover.hs
+++ test/TestMarsRover.hs
@@ -3,11 +3,24 @@ module TestMarsRover where
 
 import Test.HUnit
 
-data Direction = North | South deriving (Eq, Show)
+data Direction = North | South | West | East deriving (Eq, Show)
+
+rotateLeft :: Direction -> Direction
+rotateLeft North = West
 
 type Point = (Int, Int)
 
 type Rover = (Point, Direction)
+
+-- constructors
+
+newRover :: Point -> Direction -> Rover
+newRover p d = (p, d)
+
+defaultRover :: Rover
+defaultRover = newRover (0,0) North
+
+-- accessors
 
 position :: Rover -> Point
 position (point, _) = point
@@ -15,11 +28,10 @@ position (point, _) = point
 direction :: Rover -> Direction
 direction (_, dir) = dir
 
-newRover :: Point -> Direction -> Rover
-newRover p d = (p, d)
+-- actions
 
-defaultRover :: Rover
-defaultRover = newRover (0,0) North
+left :: Rover -> Rover
+left (pos, dir) = (pos, rotateLeft dir)
 
 
 test1 = TestCase (assertEqual "Rover is at base position" (0, 0) (position defaultRover))
@@ -30,7 +42,10 @@ test3 = TestCase (assertEqual "Rover initially faces North" North (direction def
 
 test4 = TestCase (assertEqual "Rover remembers direction" South (direction ((0, 0), South)))
 
+test5 = TestCase (assertEqual "Rover turns left from North to West" West (direction (left defaultRover)))
+
 tests = TestList [ TestLabel "test1" test1,
                    TestLabel "test2" test2,
                    TestLabel "test4" test4,
+                   TestLabel "test5" test5,
                    TestLabel "test3" test3]
