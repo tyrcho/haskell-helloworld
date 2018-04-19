664ddf01af2f6a49251936859bfe149bdc045937
[a518291] Michel Daviot <michel.daviot@atos.net>
2018-04-19 12:55:33 +0200

    :white_check_mark: trivial test for Direction

--- test/TestMarsRover.hs
+++ test/TestMarsRover.hs
@@ -3,6 +3,8 @@ module TestMarsRover where
 
 import Test.HUnit
 
+data Direction = North | South deriving (Eq, Show)
+
 type Point = (Int, Int)
 
 type Rover = Point
@@ -10,8 +12,15 @@ type Rover = Point
 position :: Rover -> Point
 position r = r
 
+direction :: Rover -> Direction
+direction _ = North
+
 test1 = TestCase (assertEqual "Rover is at base position" (0, 0) (position (0,0)))
 
 test2 = TestCase (assertEqual "Rover remembers position" (1, 2) (position (1, 2)))
 
-tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2]
+test3 = TestCase (assertEqual "Rover initially faces North" North (direction (0, 0)))
+
+tests = TestList [ TestLabel "test1" test1,
+                   TestLabel "test2" test2,
+                   TestLabel "test3" test3]
