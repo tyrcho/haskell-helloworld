be4d5b1576a73d8d27fdb813b0c3bf3d3756bbfc
[a518291] Michel Daviot <michel.daviot@atos.net>
2018-04-19 12:58:53 +0200

    :white_check_mark: remember Direction

--- test/TestMarsRover.hs
+++ test/TestMarsRover.hs
@@ -7,20 +7,23 @@ data Direction = North | South deriving (Eq, Show)
 
 type Point = (Int, Int)
 
-type Rover = Point
+type Rover = (Point, Direction)
 
 position :: Rover -> Point
-position r = r
+position (point, _) = point
 
 direction :: Rover -> Direction
-direction _ = North
+direction (_, dir) = dir
 
-test1 = TestCase (assertEqual "Rover is at base position" (0, 0) (position (0,0)))
+test1 = TestCase (assertEqual "Rover is at base position" (0, 0) (position ((0,0), North)))
 
-test2 = TestCase (assertEqual "Rover remembers position" (1, 2) (position (1, 2)))
+test2 = TestCase (assertEqual "Rover remembers position" (1, 2) (position ((1, 2),North)))
 
-test3 = TestCase (assertEqual "Rover initially faces North" North (direction (0, 0)))
+test3 = TestCase (assertEqual "Rover initially faces North" North (direction ((0, 0),North)))
+
+test4 = TestCase (assertEqual "Rover remembers direction" South (direction ((0, 0), South)))
 
 tests = TestList [ TestLabel "test1" test1,
                    TestLabel "test2" test2,
+                   TestLabel "test4" test4,
                    TestLabel "test3" test3]
