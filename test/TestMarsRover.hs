5ba383e9d7057302abadaabb7068007e3d6787e3
[a518291] Michel Daviot <michel.daviot@atos.net>
2018-04-19 12:49:43 +0200

    :white_check_mark: trivial test for position

--- test/TestMarsRover.hs
+++ test/TestMarsRover.hs
@@ -3,7 +3,11 @@ module TestMarsRover where
 
 import Test.HUnit
 
+type Rover = (Int, Int)
 
-test1 = TestCase (assertEqual "Rover is at base position" (1,2) (2,2))
+position :: Rover -> (Int, Int)
+position _ = (0, 0)
+
+test1 = TestCase (assertEqual "Rover is at base position" (0, 0) (position (0,0)))
 
 tests = TestList [TestLabel "test1" test1]
